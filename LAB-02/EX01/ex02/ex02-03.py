2f374d9c2e44b6e42630cf8ab512cac308a38cc4
thienle <minhthien26102005@gmail.com>
2026-05-28 19:54:43 +0700

    Add LAB-02 EX01

---
+++ LAB-02/EX01/ex02/ex02-03.py
@@ -0,0 +1,7 @@
+#Nhập số từ người dùng
+so = int(input("Nhập một số nguyên:"))
+#Kiểm tra xem số đó là chẵn hay lẻ
+if so % 2 == 0:
+    print(so, "là số chẵn.")
+else:
+    print(so, "không phải là số chẵn.")
